7f4479da72048a31dbd7af39697c54dedf0e7c91
Daisuke Hayashi <hysdsk.rabbit@gmail.com>
2019-01-18 20:08:39 +0900

    jsコードを簡略化

--- js/index.js
+++ js/index.js
@@ -4,10 +4,34 @@
  * @description 入力された値をローマ字に含む行データのみ表示する。
  */
 function refine (refineValue) {
-    getPrefecturesElementsArray().forEach(element => {
-        var cssDisplay = element.innerText.includes(refineValue)? "" : "none";
-        element.setAttribute("style",`display:${cssDisplay}`);
-    });
+    // tbodyの行ノード群を取得
+    var trNodeArray = document.querySelector("table#prefectures_data tbody").childNodes;
+
+    // 行の数だけ繰り返し処理
+    for (var i = 0; i < trNodeArray.length; i++) {
+
+        // 行ノードを取得
+        var trNode = trNodeArray[i];
+
+        // 絞り込み処理に不要なデータはスキップ
+        if (trNode.nodeName == "#text") {
+            continue;
+        }
+
+        // 行内のローマ字列のテキストデータを取得
+        var romaCharText = trNode.childNodes[2].innerText;
+
+        // displayのcss変数を定義
+        var cssDisplay = "";
+
+        // 入力された文字をテキストデータが含んでいない場合noneに設定
+        if (!romaCharText.includes(refineValue)) {
+            cssDisplay = "none";
+        }
+
+        // 行ノードのcssに反映
+        trNode.style.display = cssDisplay;
+    }
 }
 
 /**
@@ -24,47 +48,69 @@ function refine (refineValue) {
  */
 function sort (sortValue) {
 
+    // 未選択の場合処理終了
     if (sortValue == "0") {
-        // 未選択の場合処理終了
         return;
     }
 
-    // ソート項目と昇順or降順の取得
-    var index = Number(sortValue.split(":")[0]);
-    var isUp = sortValue.split(":")[1] == "0";
+    // 選択されたキー値を分割し項目番号と昇順判定として変数で保持
+    var sortValueArray = sortValue.split(":");
+    var index = Number(sortValueArray[0]); // 項目番号
+    var isUp = sortValueArray[1] == "0"; // 昇順判定
 
     // テーブルノードを保持
     prefectureList = [];
-    getPrefecturesElementsArray().forEach(element => {
-        var sortKey = element.childNodes[index].innerText;
+
+    var trNodeArray = document.querySelector("table#prefectures_data tbody").childNodes;
+
+    for (var i = 0; i < trNodeArray.length; i++) {
+        var trNode = trNodeArray[i]
+        if (trNode.nodeName == "#text") {
+            continue;
+        }
+
+        // ソート条件値を取得する
+        var sortKey = trNode.childNodes[index].innerText;
+
+        // ソート条件値が人口の場合数値比較するので数値に変換
         if (index == 3) {
-            // 人口の場合数値比較するので数値変換
             sortKey = Number(sortKey.replace(/,/g, ''));
         }
+
+        // 都道府県リストにソート条件値と行ノードを連想配列で保持
         prefectureList.push({
-            sortKey: sortKey,
-            node: element.cloneNode(true)
+            sortKey: sortKey, // ソート条件値
+            node: trNode.cloneNode(true) // 行ノード
         });
-    });
+
+    }
 
     // テーブルノードをソート
-    prefectureList.sort(function(a, b){
-        if (a.sortKey < b.sortKey) return isUp ? -1 : 1;
-        if (a.sortKey > b.sortKey) return isUp ? 1 : -1;
-        return 0;
+    prefectureList.sort(function(a, b) {
+        if (a.sortKey < b.sortKey) {
+            if (isUp) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (a.sortKey > b.sortKey) {
+            if (isUp) {
+                return 1
+            } else {
+                return -1;
+            }
+        } else {
+            return 0;
+        }
     });
 
-    // テーブルの再構築
+    // tbody内のHTMLを削除
     document.querySelector("table#prefectures_data tbody").innerHTML = '';
-    prefectureList.forEach(prefecture => {
-        document.querySelector("table#prefectures_data tbody").appendChild(prefecture.node);
-    });
-}
 
-/**
- * 都道府県テーブル要素取得
- * @description 都道府県テーブルのtbody以下のtr要素を配列形式で取得する。
- */
-function getPrefecturesElementsArray () {
-    return Array.prototype.slice.call(document.querySelector("table#prefectures_data tbody").children);
+    // tbody内に保持していた行ノードを追加しテーブルを再構築する
+    for (var i = 0; i < prefectureList.length; i++) {
+        var prefecture = prefectureList[i];
+        var trNode = prefecture.node;
+        document.querySelector("table#prefectures_data tbody").appendChild(trNode);
+    }
 }
