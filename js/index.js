9e31304f407f3e00408e7e6b11dbdee24146832e
Daisuke Hayashi <hysdsk.rabbit@gmail.com>
2019-01-20 21:07:16 +0900

    childNodes -> childrenに変更

--- js/index.js
+++ js/index.js
@@ -4,33 +4,23 @@
  * @description 入力された値をローマ字に含む行データのみ表示する。
  */
 function refine (refineValue) {
-    // tbodyの行ノード群を取得
-    var trNodeArray = document.querySelector("table#prefectures_data tbody").childNodes;
-
+    // tbody下の行エレメント群を取得
+    var trElements = document.querySelector("table#prefectures_data tbody").children;
+    // 行エレメント群を配列化
+    var trEletemtArray = Array.prototype.slice.call(trElements);
     // 行の数だけ繰り返し処理
-    for (var i = 0; i < trNodeArray.length; i++) {
-
-        // 行ノードを取得
-        var trNode = trNodeArray[i];
-
-        // 絞り込み処理に不要なデータはスキップ
-        if (trNode.nodeName == "#text") {
-            continue;
-        }
-
-        // 行内のローマ字列のテキストデータを取得
-        var romaCharText = trNode.childNodes[2].innerText;
-
-        // displayのcss変数を定義
+    for (var i = 0; i < trEletemtArray.length; i++) {
+        // 行エレメントを取得
+        var trEletemt = trEletemtArray[i];
+        // ローマ列エレメントを取得
+        var romanizationTdElement = trEletemt.children.item(2);
+        // ローマ列エレメントのテキストに入力値が含まれない場合noneを設定
         var cssDisplay = "";
-
-        // 入力された文字をテキストデータが含んでいない場合noneに設定
-        if (!romaCharText.includes(refineValue)) {
+        if (!romanizationTdElement.innerText.includes(refineValue)) {
             cssDisplay = "none";
         }
-
-        // 行ノードのcssに反映
-        trNode.style.display = cssDisplay;
+        // ローマ列エレメントのcssのdisplayに設定
+        trEletemtArray[i].setAttribute("style",`display:${cssDisplay}`);
     }
 }
 
@@ -58,42 +48,40 @@ function sort (sortValue) {
     var index = Number(sortValueArray[0]); // 項目番号
     var isUp = sortValueArray[1] == "0"; // 昇順判定
 
-    // テーブルノードを保持
+    // 都道府県リスト
     prefectureList = [];
-
-    var trNodeArray = document.querySelector("table#prefectures_data tbody").childNodes;
-
-    for (var i = 0; i < trNodeArray.length; i++) {
-        var trNode = trNodeArray[i]
-        if (trNode.nodeName == "#text") {
-            continue;
-        }
-
-        // ソート条件値を取得する
-        var sortKey = trNode.childNodes[index].innerText;
-
-        // ソート条件値が人口の場合数値比較するので数値に変換
+    // tbody下の行エレメント群を取得
+    var trElements = document.querySelector("table#prefectures_data tbody").children;
+    // 行エレメント群を配列化
+    var trEletemtArray = Array.prototype.slice.call(trElements);
+    // 各行のソート項目とノードを取得し行ノード保持変数に設定
+    for (var i = 0; i < trEletemtArray.length; i++) {
+        // 行エレメントを取得
+        var trEletemt = trEletemtArray[i]
+        // ソート項目テキストを行エレメントから取得
+        var sortItemText = trEletemt.children.item(index).innerText;
+        // ソート項目が人口の場合数値比較するので数値に変換
         if (index == 3) {
-            sortKey = Number(sortKey.replace(/,/g, ''));
+            sortItemText = Number(sortItemText.replace(/,/g, ''));
         }
-
         // 都道府県リストにソート条件値と行ノードを連想配列で保持
         prefectureList.push({
-            sortKey: sortKey, // ソート条件値
-            node: trNode.cloneNode(true) // 行ノード
+            item: sortItemText, // ソート項目
+            node: trEletemt.cloneNode(true) // 行ノード
         });
-
     }
 
-    // テーブルノードをソート
+    // 都道府県リストをソート
+    // ▼sort()詳細
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     prefectureList.sort(function(a, b) {
-        if (a.sortKey < b.sortKey) {
+        if (a.item < b.item) {
             if (isUp) {
                 return -1;
             } else {
                 return 1;
             }
-        } else if (a.sortKey > b.sortKey) {
+        } else if (a.item > b.item) {
             if (isUp) {
                 return 1
             } else {
@@ -106,7 +94,6 @@ function sort (sortValue) {
 
     // tbody内のHTMLを削除
     document.querySelector("table#prefectures_data tbody").innerHTML = '';
-
     // tbody内に保持していた行ノードを追加しテーブルを再構築する
     for (var i = 0; i < prefectureList.length; i++) {
         var prefecture = prefectureList[i];
